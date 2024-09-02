6da2d940a5b235a8f31347bfcd9842645f27e043
Nicola Schneider <nicola@nicolaschneider.ch>
2024-09-02 11:59:49 +0200

    Updated BookingState

--- src/utils/BookingState.ts
+++ src/utils/BookingState.ts
@@ -1,6 +1,56 @@
-const BookingState: Record<string, 'AUSSTEHEND' | "BESTÄTIGT" | "ABGELEHNT"> = {
+type BookingStateColors = 'default' | 'success' | 'warning' | 'danger'
+
+const BookingState: Record<string, 'AUSSTEHEND' | 'BESTÄTIGT' | 'ABGELEHNT' | 'UNBEKANNT'> = {
     PENDING: 'AUSSTEHEND',
     CONFIRMED: 'BESTÄTIGT',
     CANCELLED: 'ABGELEHNT',
+    UNKNOWN: 'UNBEKANNT',
 }
-export default BookingState
+
+const BookingStateColor: Record<string, 'danger' | 'success' | 'warning' | 'default'> = {
+    PENDING: 'warning',
+    CONFIRMED: 'success',
+    CANCELLED: 'danger',
+    UNKNOWN: 'default',
+}
+
+const BookingStateStatus: Record<string, string> = {
+    PENDING: 'Anfrage wird geprüft',
+    CONFIRMED: 'Anfrage wurde bestätigt',
+    CANCELLED: 'Anfrage wurde abgelehnt',
+    UNKNOWN: 'Staus der Anfrage unbekannt',
+}
+
+function getColorByValue(value: string): BookingStateColors {
+    switch (value) {
+        case BookingState.PENDING:
+            return 'warning'
+        case BookingState.CONFIRMED:
+            return 'success'
+        case BookingState.CANCELLED:
+            return 'danger'
+        default:
+            return 'default'
+    }
+}
+
+function getKeyByValue(value: 'AUSSTEHEND' | 'BESTÄTIGT' | 'ABGELEHNT' | 'UNBEKANNT' | string): string {
+    const key = Object.keys(BookingState).find((key) => BookingState[key] === value)
+    if (key)
+        return key
+    return BookingState.UNKNOWN
+}
+
+const BookingDateFilter: Record<string, 'ALTE EINTRÄGE' | 'NEUE EINTRÄGE'>  = {
+    OLD_ENTRIES: 'ALTE EINTRÄGE',
+    NEW_ENTRIES: 'NEUE EINTRÄGE',
+}
+
+function getDateFilterKey(value: 'ALTE EINTRÄGE' | 'NEUE EINTRÄGE' | string): string {
+    const key = Object.keys(BookingDateFilter).find((key) => BookingDateFilter[key] === value)
+    if (key)
+        return key
+    return 'all'
+}
+
+export {BookingState, BookingStateColor, BookingStateStatus, getColorByValue, getKeyByValue, BookingDateFilter, getDateFilterKey}
